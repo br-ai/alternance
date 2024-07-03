1ac9e61ad2f2313525d5def70e70989af3eaec5a
br-ai <brandonsitedjeya@.com>
2024-07-03 14:56:45 +0200

    feat: initialize project

---
+++ home/header.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>shared center contact</title>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/7.2.96/css/materialdesignicons.min.css" integrity="sha512-LX0YV/MWBEn2dwXCYgQHrpa9HJkwB+S+bnBpifSOTO1No27TqNMKYoAn6ff2FBh03THAzAiiCwQ+aPX+/Qt/Ow==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="style.css" rel="stylesheet">
+    <link rel="icon" href="../img/logo.png">
+</head>
+<body>
+    
+<!-- Header -->
+<div id="top-nav" class="navbar navbar-static-top">
+  <div class="container bootstrap snippets bootdey">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon-toggle"></span>
+      </button>
+      <a class="navbar-brand" href="home.php"><img src="../img/logo.png" class="img-logo" alt="logo PoleIt"></a>
+    </div>
+    <div class="navbar-collapse collapse">
+      <ul class="nav navbar-nav navbar-right">
+        
+        <li class="dropdown">
+          <a class="dropdown-toggle" role="button" data-toggle="dropdown" href="#">
+            <i class="glyphicon glyphicon-user"></i> <?php echo $user['username']?> </a>
+          <ul id="g-account-menu" class="dropdown-menu" role="menu">
+            
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div><!-- /container -->
+</div>
+<!-- /Header -->
+
+<!-- Main -->
+<div class="container bootstrap snippets bootdey">
+  
+  <!-- upper section -->
+  <div class="row">
+    <div class="col-md-3">
+      <!-- left -->
+      <a href="#"><strong><i class="glyphicon glyphicon-briefcase"></i> <?php echo $user['username']?></strong></a>
+      <hr>
+      
+      <ul class="nav nav-pills nav-stacked">
+        <li><a href="home.php" class="active"><i class="glyphicon glyphicon-flash"></i> Dashboard</a></li>
+        <li><a href="users.php"><i class="glyphicon glyphicon-user"></i> Users</a></li>
+        <li><a href="blog.php"><i class="glyphicon glyphicon-list-alt"></i> Blogs</a></li>
+        <li><a href="actualites.php"><i class="glyphicon glyphicon-book"></i> Actualités</a></li>
+        <li><a href="category.php"><i class="glyphicon glyphicon-book"></i> Categories</a></li>
+        <li><a href="actions.php"><i class="glyphicon glyphicon-briefcase"></i> Actions</a></li>
+        
+      </ul>
+
+      <a href="../index.php"><button type="submit" class="btn btn-danger w-33" id="submitButton" ><i class="fa fa-sign-out" aria-hidden="true"></i> Deconnexion</button></a>
+      
+      <hr>
+      
+  	</div><!-- /span-3 -->
+      <div class="col-md-9">   	
+      <!-- column 2 -->	
+       <a href="#"><strong><i class="glyphicon glyphicon-dashboard"></i> </strong></a>     
+       <hr>
+	   <div class="row">
+            <!-- center left-->	
+         	<div class="col-md-12">
+			  <div class="well">
+
+        </div>
+              
+              <hr>
---
+++ home/home.php
@@ -0,0 +1,49 @@
+<?php include 'header.php';?>
+
+              
+              <div class="panel panel-default">
+                  <div class="panel-heading"><h4>Vue globale</h4></div>
+                  <div class="global_view">
+                    <div class="item_global_view">
+                        <i class="fa fa-users"></i>
+                        <p>
+                       
+                       80 Utilisateurs </p>
+                    </div>
+                    <div class="item_global_view">
+                        <i class="glyphicon glyphicon-list-alt"></i>
+                        <p>
+                     10 blogs </p>
+                    </div>
+                    <div class="item_global_view">
+                        <i class="fa fa-comments"></i>
+                        <p>
+                        10 commentaires </p>
+                    </div>
+                    <div class="item_global_view">
+                        <i class="glyphicon glyphicon-book"></i>
+                        <p>10 Actualités </p>
+                    </div>
+                    <div class="item_global_view">
+                        <i class="glyphicon glyphicon-list-alt"></i>
+                        <p>10 Categories </p>
+                    </div>
+                    
+                    
+
+                  </div><!--/panel-body-->
+              </div><!--/panel-->                     
+              
+          	</div><!--/col-->
+         
+           
+       </div><!--/row-->
+  	</div><!--/col-span-9-->
+  </div><!--/row-->
+  <!-- /upper section -->
+</div><!--/container-->
+<!-- /Main -->
+
+
+</body>
+</html>
